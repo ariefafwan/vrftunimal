33cf82cecd1aef098c43c53a3f029f7aef343e79
Afwan <tmariefafwan@gmail.com>
2022-06-28 01:16:05 +0700

    sampe login sama migrasi

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,8 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->bigInteger('role_id')->default('2')->unsigned();
+            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade')->onUpdate('cascade');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
