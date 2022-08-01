6153b23eab719aecb16cc0d5538a76d3c44c557c
Afwan <tmariefafwan@gmail.com>
2022-08-02 03:12:31 +0700

    dah front end nya berita

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Models\Berita;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
 
 class AdminController extends Controller
 {
@@ -14,5 +16,11 @@ class AdminController extends Controller
         $user = Auth::user();
         $page = "Dasboard Admin";
         return view('admin.dashboard', compact('user', 'page'));
+    }
+
+    public function welcome()
+    {
+        $berita = Berita::all();
+        return view('index', compact('berita'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -21,9 +21,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::get('/', [AdminController::class, 'welcome']);
 
 Auth::routes();
 Route::middleware(['auth'])->group(function () {
