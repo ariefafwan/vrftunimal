3cb7d3413384d7f7cf21286b865ebe3b9322f774
Afwan <tmariefafwan@gmail.com>
2022-08-12 01:55:37 +0700

    asik merapikan aja pun (sejarah, visi misi)

---
+++ resources/views/appnav.blade.php
@@ -0,0 +1,113 @@
+<nav id="navbar" class="navbar">
+    <ul>
+      <li><a class="nav-link" href="{{ route('welcome') }}">Beranda</a></li>
+      <li class="dropdown"><a href="#"><span>Profil</span> <i class="fa fa-angle-down" aria-hidden="true"></i></a>
+        <ul>
+          <li><a href="{{ route('sejarah') }}">Sejarah</a></li>
+          <li><a href="{{ route('visimisi') }}">Visi Misi</a></li>
+          <li><a href="#team">Struktur Organisasi</a></li>
+          <li><a href="#">Profil Pimpinan Fakultas</a></li>
+          <li><a href="#">Fasilitas Penunjang</a></li>
+          <li><a href="#">Prestasi</a></li>
+          <li><a href="#">Dosen</a></li>
+          <li><a href="#">Tenaga Kependidikan</a></li>
+        </ul>
+      </li>
+      <li class="dropdown"><a href="#"><span>Jurusan</span> <i class="fa fa-angle-down" aria-hidden="true"></i></a>
+        <ul>
+          <li class="dropdown"><a href="#"><span>Jurusan Teknik Sipil</span><i class="fa fa-angle-down" aria-hidden="true"></i></a>
+            <ul>
+              <li><a href="#">Prodi Teknik Sipil</a></li>
+              <li><a href="#">Prodi Teknik Arsitektur</a></li>
+              <li><a href="#">Prodi Magister Teknik Sipil</a></li>
+            </ul>
+          </li>
+          <li class="dropdown"><a href="#"><span>Jurusan Teknik Mesin</span><i class="fa fa-angle-down" aria-hidden="true"></i></a>
+            <ul>
+              <li><a href="#">Prodi Teknik Mesin</a></li>
+              <li><a href="#">Prodi Magister Teknik Energi Tebarukan</a></li>
+            </ul>
+          </li>
+          <li class="dropdown"><a href="#"><span>Jurusan Teknik Industri</span><i class="fa fa-angle-down" aria-hidden="true"></i></a>
+            <ul>
+              <li><a href="#">Prodi Teknik Industri</a></li>
+              <li><a href="#">Prodi Teknik Logistik</a></li>
+            </ul>
+          </li>
+          <li class="dropdown"><a href="#"><span>Jurusan Teknik Kimia</span><i class="fa fa-angle-down" aria-hidden="true"></i></a>
+            <ul>
+              <li><a href="#">Prodi Teknik Kimia</a></li>
+              <li><a href="#">Prodi Teknik Material</a></li>
+            </ul>
+          </li>
+          <li class="dropdown"><a href="#"><span>Jurusan Teknik Elektro</span><i class="fa fa-angle-down" aria-hidden="true"></i></a>
+            <ul>
+              <li><a href="#">Prodi Teknik Elektro</a></li>
+              <li><a href="#">Prodi Sistem Informasi</a></li>
+            </ul>
+          </li>
+          <li class="dropdown"><a href="#"><span>Jurusan Teknik Informatika</span><i class="fa fa-angle-down" aria-hidden="true"></i></a>
+            <ul>
+              <li><a href="#">Prodi Teknik Informatika</a></li>
+              <li><a href="#">Prodi Magister Teknologi Informasi</a></li>
+            </ul>
+          </li>
+        </ul>
+      </li>
+      <li class="dropdown"><a href="#"><span>Akademik</span> <i class="fa fa-angle-down" aria-hidden="true"></i></a>
+        <ul>
+          <li><a href="#">Informasi Akademik</a></li>
+          <li class="dropdown"><a href="#"><span>Penelitian & Pengabdian</span><i class="fa fa-angle-down" aria-hidden="true"></i></a>
+            <ul>
+              <li><a href="#">LPPM</a></li>
+              <li><a href="#">Penelitian</a></li>
+              <li><a href="#">Pengabdian</a></li>
+              <li><a href="#">HaKI</a></li>
+            </ul>
+          </li>
+          <li><a href="#">Daftar Yudisium</a></li>
+          <li><a href="#">Kebijakan Akademik</a></li>
+          <li><a href="#">Panduan Akademik</a></li>
+          <li><a href="#">Akreditasi</a></li>
+          <li><a href="#">Informasi Umum</a></li>
+        </ul>
+      </li>
+      <li class="dropdown"><a href="#"><span>Layanan</span> <i class="fa fa-angle-down" aria-hidden="true"></i></a>
+        <ul>
+          <li><a href="#">SIA</a></li>
+          <li><a href="#">E-Learning</a></li>
+          <li><a href="#">E-Library</a></li>
+          <li><a href="#">SIMPEG</a></li>
+          <li><a href="#">PORTAL</a></li>
+          <li><a href="#">Survey UNIMAL</a></li>
+          <li><a href="#">Layanan Kemahasiswaan</a></li>
+        </ul>
+      </li>
+      <li class="dropdown"><a href="#"><span>Kemahasiswaan</span> <i class="fa fa-angle-down" aria-hidden="true"></i></a>
+        <ul>
+          <li><a href="#">Kampus Merdeka</a></li>
+          <li class="dropdown"><a href="#"><span>Organisasi Mahasiswa</span><i class="fa fa-angle-down" aria-hidden="true"></i></a>
+            <ul>
+              <li><a href="#">BEM FT UNIMAL</a></li>
+              <li><a href="#">DPM FT UNIMAL</a></li>
+              <li><a href="#">HIMATESIP</a></li>
+              <li><a href="#">HIMATESIN</a></li>
+              <li><a href="#">HIMTI</a></li>
+              <li><a href="#">HIMATEMIA</a></li>
+              <li><a href="#">HIMATRO</a></li>
+              <li><a href="#">HIMATEKA</a></li>
+              <li><a href="#">HIMATIF</a></li>
+              <li><a href="#">HIMASI</a></li>
+              <li><a href="#">HIMATERIAL</a></li>
+              <li><a href="#">Teknik Logistik</a></li>
+            </ul>
+          </li>
+          <li><a href="#">Alumni</a></li>
+          <li><a href="#">Data Mahasiswa</a></li>
+          <li><a href="#">Prestasi Mahasiswa</a></li>
+          <li><a href="#">Kegiatan Mahasiswa</a></li>
+        </ul>
+      </li>
+    </ul>
+    <i class="fa fa-bars mobile-nav-toggle"></i>
+  </nav><!-- .navbar -->
--- resources/views/sejarah.blade.php
+++ resources/views/sejarah.blade.php
@@ -1,104 +1,126 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-    <meta name="generator" content="Hugo 0.101.0">
-    <title>{{ $page }} · Bootstrap v5.2</title>
 
-    <link rel="canonical" href="https://getbootstrap.com/docs/5.2/examples/headers/">
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-    
+  <title>{{ $page }} | Fakultas Teknik - UNIMAL</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
 
-    
+  <!-- Favicons -->
+    <link rel="apple-touch-icon" sizes="57x57" href="favicon/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="favicon/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="favicon/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="favicon/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="favicon/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="favicon/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="favicon/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="favicon/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="favicon/android-192x192.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="favicon/favicon-96x96.png">
+    <link rel="manifest" href="favicon/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="favicon/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+    <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
+    <link rel="manifest" href="favicon/site.webmanifest">
 
-<link href="css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/35770c1bfd.js" crossorigin="anonymous"></script>
+  
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
-    <style>
-      .bd-placeholder-img {
-        font-size: 1.125rem;
-        text-anchor: middle;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        user-select: none;
-      }
+  <!-- Vendor CSS Files -->
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link href="css/bootstrap-icons.css" rel="stylesheet">
+  <link href="css/boxicons.min.css" rel="stylesheet">
+  <link href="css/glightbox.min.css" rel="stylesheet">
+  <link href="css/swiper-bundle.min.css" rel="stylesheet">
 
-      @media (min-width: 768px) {
-        .bd-placeholder-img-lg {
-          font-size: 3.5rem;
-        }
-      }
+  <!-- Template Main CSS File -->
+  <link href="css/sejarah.css" rel="stylesheet">
 
-      .b-example-divider {
-        height: 3rem;
-        background-color: rgba(0, 0, 0, .1);
-        border: solid rgba(0, 0, 0, .15);
-        border-width: 1px 0;
-        box-shadow: inset 0 .5em 1.5em rgba(0, 0, 0, .1), inset 0 .125em .5em rgba(0, 0, 0, .15);
-      }
+</head>
 
-      .b-example-vr {
-        flex-shrink: 0;
-        width: 1.5rem;
-        height: 100vh;
-      }
+<body>
 
-      .bi {
-        vertical-align: -.125em;
-        fill: currentColor;
-      }
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top header">
+    <div class="container d-flex align-items-center justify-content-between">
 
-      .nav-scroller {
-        position: relative;
-        z-index: 2;
-        height: 2.75rem;
-        overflow-y: hidden;
-      }
+      <a href="https://teknik.unimal.ac.id/"><img class="logo col-md-10 col-xs-6" src="https://teknik.unimal.ac.id/vic_image/system/logo_unimal_2.png"></a>
+      <div class="header-tengah col-md-3 col-sm-6 col-xs-10">
+        <span> <b class="judul-web text-white">Fakultas Teknik</span>
+        <br>
+        <span class="deskripsi-web text-white">Universitas Malikussaleh</span>
+      </div>
 
-      .nav-scroller .nav {
-        display: flex;
-        flex-wrap: nowrap;
-        padding-bottom: 1rem;
-        margin-top: -1px;
-        overflow-x: auto;
-        text-align: center;
-        white-space: nowrap;
-        -webkit-overflow-scrolling: touch;
-      }
-    </style>
+      @include('appnav')
 
-    
-    <!-- Custom styles for this template -->
-    <link href="css/headers.css" rel="stylesheet">
-  </head>
-  <body>
+    </div>
+  </header><!-- End Header -->
 
-<main>
-  <h1 class="visually-hidden">Headers examples</h1>
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="d-flex align-items-center justify-content-center">
+    <div class="container position-relative">
+      <h1>Sejarah</h1>
+      <h2>Fakultas Teknik Universitas Malikussaleh</h2>
+      <a href="" class="btn-get-started mt-4"><i class="fa fa-circle-play" aria-hidden="true"></i>  Mulai VR Tour</a>
+      {{-- <a href="https://www.youtube.com/watch?v=jDDaplaOz7Q" class="btn-watch-started"><i class="bi bi-play-circle"></i><span>Watch Video</span></a> --}}
+    </div>
+  </section><!-- End Hero -->
 
-  <div class="container">
-    <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-      <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
-        <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"/></svg>
-        <span class="fs-4">Simple header</span>
-      </a>
-      <ul class="nav nav-pills">
-        <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Home</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">Features</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">Pricing</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">FAQs</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">About</a></li>
-      </ul>
-    </header>
-  </div>
+  <main id="main">
 
-</main>
+    <!-- ======= Team Section ======= -->
+    <section id="team" class="team section-bg">
+        <div class="container" data-aos="fade-up">
+  
+          <div class="section-title text-primary">
+            <h2>Sejarah</h2>
+            <h5 class="text-start text-dark">Pada tahun 1982 didirikan Sekolah Tinggi Teknik Malikussaleh yang berada dibawah koordinasi Yayasan Pendidikan Malikussaleh, dimana Jurusan yang pertama sekali didirikan adalah Jurusan Teknik Sipil & Teknik Mesin, dan kemudian pada tahun 1983 dibuka Jurusan Teknik & Manajemen Industri.</h5>
+            <br>
+            <h5 class="text-start text-dark">Untuk meningkatkan status maka pada tanggal 19 Maret 1984 secara resmi Sekolah Tinggi Teknik Malikussaleh berstatus Terdaftar dengan Surat Keputusan Menteri Pendidikan dan Kebudayaan RI dengan nomor 0357/0/1984.  Pada tahun 1996  Sekolah Tinggi Teknik Malikussaleh Membuka satu jurusan baru yaitu Jurusan Teknik Kimia.</h5>
+            <br>
+            <h5 class="text-start text-dark">Upaya peningkatan ruang lingkup kerja tidak hanya sampai disitu, karena pada tanggal 11 September 1989 secara resmi Sekolah Tinggi Teknik Malikussaleh terdaftar sebagai FT UNIMAL dengan Surat Keputusan Menteri Pendidikan dan Kebudayaan RI No. 0584/0/1989 dengan jurusannya Teknik Sipil, Teknik Mesin, Teknik & Manajemen Industri dan Teknik Kimia.</h5>
+            <br>
+            <h5 class="text-start text-dark">Dengan melihat perkembangan dan tingkat kebutuhan  pendidikan Teknik dikawasan Industri Lhokseumawe di tahun 1989 Sekolah Tinggi Teknik Malikussaleh kembali membuka Jurusan Teknik Elektro.</h5>
+            <h5 class="text-start text-dark">Perkembangan ini telah membawa FT UNIMAL memiliki lima Jurusan  yaitu :</h5>
+            <h5 class="text-start text-dark">1. Jurusan Teknik Sipil. </h5>
+            <h5 class="text-start text-dark">2. Jurusan Teknik Mesin. </h5>
+            <h5 class="text-start text-dark">3. Jurusan Teknik Industri. </h5>
+            <h5 class="text-start text-dark">4. Jurusan Teknik Kimia. </h5>
+            <h5 class="text-start text-dark">5. Jurusan Teknik Elektro. </h5>
+            <br>
+            <h5 class="text-start text-dark"> Pengelolaan Fakultas Teknik sejak tahun 1987 sampai tahun 1996 dipercayakan oleh Yayasan Pendidikan Malikussaleh kepada PERSATUAN INSINYUR INDONESIA (PII) Cabang Lhokseumawe, selanjutnya setelah itu kembali lagi ke Yayasan.</h5>
+            <br>
+            <h5 class="text-start text-dark">Dengan penegerian UNIMAL pada tanggal 01 Agustus 2001 Oleh presiden Megawati Soekarno Putri berdasarkan Keppres N0. 95 Tahun 2001, FT UNIMAL terus berusaha untuk memenuhi kebutuhan perkembangan Teknologi maka di tahun ajaran 2003-2004 membuka lagi dua prodi baru yaitu Program Studi Informatika yang berada dibawah Jurusan Teknik elektro dan Program Studi Teknik Arsitektur dibawah Jurusan Teknik sipil. Kemudian pada tahun 2014 membuka prodi baru yaitu Program Studi Sistem Informasi.</h5>
+          </div>
+  
+        </div>
+      </section><!-- End Team Section -->
+    </main><!-- End #main -->
+  
+    @include('appfooter')
 
+    <div id="preloader"></div>
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+  <!-- Vendor JS Files -->
+  <script src="js/purecounter_vanilla.js"></script>
+  <script src="js/bootstrap.bundle.min.js"></script>
+  <script src="js/glightbox.min.js"></script>
+  <script src="js/isotope.pkgd.min.js"></script>
+  <script src="js/swiper-bundle.min.js"></script>
+  <script src="js/validate.js"></script>
 
-    <script src="js/bootstrap.bundle.min.js"></script>
+  <!-- Template Main JS File -->
+  <script src="js/main.js"></script>
 
-      
-  </body>
+</body>
+
 </html>
